ef49bf07aae5dfa4083ef5717687063ed9c25bfa
Unilfo <kokopal13@gmail.com>
2020-05-26 17:01:31 +0300

    3

---
+++ build/precache-manifest.9afab0579717d02bfee4c9e6f312e168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc827ef4512000ec77197e206bc2fa4",
+    "url": "/data-table-phone/index.html"
+  },
+  {
+    "revision": "28dcee404a4ec6329b2d",
+    "url": "/data-table-phone/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c053149ab27ebfc3c54c",
+    "url": "/data-table-phone/static/css/main.816b3787.chunk.css"
+  },
+  {
+    "revision": "28dcee404a4ec6329b2d",
+    "url": "/data-table-phone/static/js/2.96e0b4c2.chunk.js"
+  },
+  {
+    "revision": "b5854a3dcd4b31b7fde94252c473317a",
+    "url": "/data-table-phone/static/js/2.96e0b4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c053149ab27ebfc3c54c",
+    "url": "/data-table-phone/static/js/main.d8649f9e.chunk.js"
+  },
+  {
+    "revision": "187a739ab38b420ae816",
+    "url": "/data-table-phone/static/js/runtime-main.19e77a18.js"
+  }
+]);
