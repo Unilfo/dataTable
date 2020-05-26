6a9f2a02b297247ec50927646bac2f2a1d402525
Unilfo <kokopal13@gmail.com>
2020-05-26 17:20:08 +0300

    Updates

---
+++ precache-manifest.abf8335148da22bbfd0aeefb3e8d92e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee4fe953ecb6943fb61306ee8c2cf3e",
+    "url": "/dataTable/index.html"
+  },
+  {
+    "revision": "9af02ad3353bb9145041",
+    "url": "/dataTable/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c053149ab27ebfc3c54c",
+    "url": "/dataTable/static/css/main.816b3787.chunk.css"
+  },
+  {
+    "revision": "9af02ad3353bb9145041",
+    "url": "/dataTable/static/js/2.bf9459f7.chunk.js"
+  },
+  {
+    "revision": "b5854a3dcd4b31b7fde94252c473317a",
+    "url": "/dataTable/static/js/2.bf9459f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c053149ab27ebfc3c54c",
+    "url": "/dataTable/static/js/main.d8649f9e.chunk.js"
+  },
+  {
+    "revision": "33d6aac02589cf3f2a54",
+    "url": "/dataTable/static/js/runtime-main.d101fa7c.js"
+  }
+]);
